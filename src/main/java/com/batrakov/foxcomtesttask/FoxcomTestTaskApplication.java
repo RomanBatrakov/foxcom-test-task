42004d47a76cdc3d877a6a43a3a61fa11f58ae2d
Roman_B <batrakov.r.v@yandex.ru>
2023-04-03 14:54:35 +0300

    empty work app

---
+++ src/main/java/com/batrakov/foxcomtesttask/FoxcomTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.batrakov.foxcomtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxcomTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxcomTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batrakov/foxcomtesttask/FoxcomTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batrakov.foxcomtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxcomTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
